745603ded1dbe778d11b2541961f2edacc9a8790
Julian Grisales <julygrisales@gmail.com>
2024-09-12 17:35:14 -0500

    Se actualiza PerimeterC en el Main

--- src/main/java/PerimeterCalculator.java
+++ src/main/java/PerimeterCalculator.java
@@ -1,6 +1,7 @@
 import org.talento.tech.*;
 
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Este codigo permite calcular el perimetro de figuras de N >= 3 lados
@@ -14,36 +15,46 @@ public class PerimeterCalculator {
     public PerimeterCalculator () {}
 
     public double calculatePerimeter(Shape s) {
-        // Definir la variable que acumule el perimetro
+        int perimeter = 0;
 
+        // Definir la variable que acumule el perimetro
         List<Point> points = s.getPoints();
         Point prevPoint = points.getLast();
 
         for (Point point : points) {
             // Obtener la distancia de los lados
             // Ayuda: prevPoint.distancia(point)
+            double distance = prevPoint.distancia(point);
+            perimeter += distance;
 
             prevPoint = point;
         }
 
         // Retornar la variable del perimetro
+        return perimeter;
     }
 
     public int getTotalPoints(Shape s) {
         // Definir la variable para el total de puntos
+        int totalPoints = 0;
+
         for(Point point: s.getPoints()) {
             // Acumular el total de puntos
+            totalPoints++;
+
         }
 
         // Retornas el valor de puntos
+        return totalPoints;
     }
 
     public static void main (String[] args) {
         PerimeterCalculator perimeterCalculator = new PerimeterCalculator();
-        // Definit el nombre del archivo
+        // Defini el nombre del archivo
+        String filename = "square.txt";
         Shape shape = new Shape();
 
-        shape.readPoints("nombre_archivo");
+        shape.readPoints(filename);
         // Mostrar el valor de calculatePerimeter y getTotalPoints
     }
 }
